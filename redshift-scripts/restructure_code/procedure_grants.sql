44338d5eb1d15a5148ec68fba0c5b80f4728ab24
psodhi-gds <paramjeet.sodhi@digital.cabinet-office.gov.uk>
2024-02-14 13:06:49 +0000

    add re-structured code

---
+++ redshift-scripts/restructure_code/procedure_grants.sql
@@ -0,0 +1,10 @@
+GRANT EXECUTE ON procedure conformed_refactored.update_dap_data_mart() TO "IAMR:dev-dap-redshift-processing-role";
+GRANT EXECUTE ON procedure conformed_refactored.dim_event_upsert() TO "IAMR:dev-dap-redshift-processing-role";
+GRANT EXECUTE ON procedure conformed_refactored.dim_journey_channel_refactored_upsert() TO "IAMR:dev-dap-redshift-processing-role";
+GRANT EXECUTE ON procedure conformed_refactored.dim_relying_party_refactored_upsert() TO "IAMR:dev-dap-redshift-processing-role";
+GRANT EXECUTE ON procedure conformed_refactored.dim_user_journey_event_upsert() TO "IAMR:dev-dap-redshift-processing-role";
+GRANT EXECUTE ON procedure conformed_refactored.dim_user_refactored_upsert() TO "IAMR:dev-dap-redshift-processing-role";
+GRANT EXECUTE ON procedure conformed_refactored.event_extensions_refactored_upsert() TO "IAMR:dev-dap-redshift-processing-role";
+GRANT EXECUTE ON procedure conformed_refactored.fact_user_journey_event_refactored_upsert() TO "IAMR:dev-dap-redshift-processing-role";
+GRANT EXECUTE ON procedure conformed_refactored.redshift_date_dim() TO "IAMR:dev-dap-redshift-processing-role";
+GRANT EXECUTE ON procedure conformed_refactored.update_event_batch_table() TO "IAMR:dev-dap-redshift-processing-role";
---
+++ redshift-scripts/restructure_code/ref_relying_parties_inserts_refactored.sql
@@ -0,0 +1,49 @@
+
+/*insert into conformed_refactored.ref_relying_parties_refactored values (1,'RqFZ83csmS4Mi4Y7s7ohD9-ekwU','basic DBS check','DBS');
+insert into conformed_refactored.ref_relying_parties_refactored values (2,'XwwVDyl5oJKtK0DVsuw3sICWkPU','Vehicle Operator Licence','DVSA');
+insert into conformed_refactored.ref_relying_parties_refactored values (3,'VsAkrtMBzAosSveAv4xsuUDyiSs','Sign your mortgage deed','HMLR');
+insert into conformed_refactored.ref_relying_parties_refactored values (4,'7y-bchtHDfucVR5kcAe8KaM80wg','gds-iv-gateway','HMRC');
+insert into conformed_refactored.ref_relying_parties_refactored values (5,'pDqO7_Hu-pq5wam5I4MlURXrv5k','Modern Slavery Statement Register','MSU');
+insert into conformed_refactored.ref_relying_parties_refactored values (6,'TGygWFxGDNn8ItyaecWCopqIX3s','Subject matter specialists Ofqual','OFQUAL');
+insert into conformed_refactored.ref_relying_parties_refactored values (7,'-1','Other','Other');
+insert into conformed_refactored.ref_relying_parties_refactored values (8,'LUIZbIuJ_xVZxwhkNAApcO4O_6o','Social Work England','SWE');
+insert into conformed_refactored.ref_relying_parties_refactored values (9,'x3F_Iu0LgqJpegY5ni0QSB0uezw','Apprenticeship Service','Apprenticeship-Service');
+insert into conformed_refactored.ref_relying_parties_refactored values (10,'bGAwNKM0XvnxCAuDQ_rMhhP3dxM','Claim Criminal Injuries Compensation','CCIC');
+insert into conformed_refactored.ref_relying_parties_refactored values (11,'Y6YaRZ9bjCwS6HxaB34zvRhZJgBQyryT','di-auth-stub-relying-party-production','di-auth-stub-relying-party-production');
+insert into conformed_refactored.ref_relying_parties_refactored values (12,'cAcut5r3PMtoDORZr5b9JT7VPw6VuF3Q','di-auth-stub-relying-party-production-app','di-auth-stub-relying-party-production-app');
+insert into conformed_refactored.ref_relying_parties_refactored values (13,'LcueBVCnGZw-YFdTZ4S07XbQx7I','GOV.UK','GOV');
+insert into conformed_refactored.ref_relying_parties_refactored values (14,'OdwbXmA5NLlYmMGHy3kjKFVD3PQ','Identity Proving and Verification Return (IPV Return) - Production','IPV');
+insert into conformed_refactored.ref_relying_parties_refactored values (15,'ZL0kvRBP5xMy5OwONj8ARLPyuko','lite-exporter','LITE');
+insert into conformed_refactored.ref_relying_parties_refactored values (16,'KcKmx2g1GH6ersWFvzMi1bhehq4','production-account-management','production-account-management');
+insert into conformed_refactored.ref_relying_parties_refactored values (17,'MjQc1h7nFVbNM05iawAdkkZ2W89uloDK','di-auth-smoketest-microclient-production','SMOKE TEST');
+insert into conformed_refactored.ref_relying_parties_refactored values (18,'zFeCxrwpLCUHFm-C4_CztwWtLfQ','Veterans','Veterans');
+insert into conformed_refactored.ref_relying_parties_refactored values (19,'tya4DoMpw_B7FK5YvuMAj3asc0A','Find a Grant','GMF');*/
+
+
+INSERT INTO conformed_refactored.REF_RELYING_PARTIES_refactored(CLIENT_ID,CLIENT_NAME,DISPLAY_NAME) 
+    VALUES
+('RqFZ83csmS4Mi4Y7s7ohD9-ekwU',	'Basic Digital Service','DBS - Basic Digital Service'),
+('XwwVDyl5oJKtK0DVsuw3sICWkPU','Vehicle Operator Licence','DVSA - Vehicle Operator Licence'),
+('VsAkrtMBzAosSveAv4xsuUDyiSs',	'Sign Your Mortgage Deed','HMLR - Sign Your Mortgage Deed'),
+('7y-bchtHDfucVR5kcAe8KaM80wg',	'Government Gateway','HMRC - Government Gateway'),
+('pDqO7_Hu-pq5wam5I4MlURXrv5k',	'Modern Slavery Statement Register','MSU - Modern Slavery Statement Register'),
+('TGygWFxGDNn8ItyaecWCopqIX3s',	'Subject Matter Specialist','Ofqual - Subject Matter Specialist'),
+('-1',	'Other','Other - Other'),
+('LUIZbIuJ_xVZxwhkNAApcO4O_6o',	'Social Worker Register','SWE - Social Worker Register'),
+('x3F_Iu0LgqJpegY5ni0QSB0uezw',	'Apprenticeship Service','NAS - Apprenticeship Service'),
+('bGAwNKM0XvnxCAuDQ_rMhhP3dxM',	'Claim Criminal Injuries Compensation (CCIC)','CICA - Claim Criminal Injuries Compensation'),
+('Y6YaRZ9bjCwS6HxaB34zvRhZJgBQyryT','di-auth-stub-relying-party-production','di-auth-stub - di-auth-stub-relying-party-production'),
+('cAcut5r3PMtoDORZr5b9JT7VPw6VuF3Q','di-auth-stub-relying-party-production-app','di-auth-stub - di-auth-stub-relying-party-production-app'),
+('LcueBVCnGZw-YFdTZ4S07XbQx7I',	'GOV.UK','GOV - GOV.UK'),
+('OdwbXmA5NLlYmMGHy3kjKFVD3PQ',	'Identity Proving and Verification Return (IPV Return) - Production','IPV - Identity Proving and Verification Return - Production'),
+('ZL0kvRBP5xMy5OwONj8ARLPyuko',	'Licensing for International Trade and Enterprise','DBT - Licensing for International Trade and Enterprise (LITE)'),
+('KcKmx2g1GH6ersWFvzMi1bhehq4',	'production-account-management','production-account-management - production-account-management'),
+('MjQc1h7nFVbNM05iawAdkkZ2W89uloDK','di-auth-smoketest-microclient-production','SMOKE TEST - di-auth-smoketest-microclient-production'),
+('zFeCxrwpLCUHFm-C4_CztwWtLfQ',	'Veterans Identity Card','Veterans - Veterans Identity Card'),
+('tya4DoMpw_B7FK5YvuMAj3asc0A',	'Find and Apply for a Grant','GMF - Find and Apply for a Grant'),
+('9fduJ6KAE8WwCb1VCKp788BC8mM',	'UK Market Conformity Assessment Bodies (UKMCAB)','OPSS - UK Market Conformity Assessment Bodies'),
+('zbNToJPcre4BXEap0na8kOjniKg',	'Manage Family Hubs','DFE - Manage Family Hubs'),
+('FakIq5aYsHQ02dBOc6XwyA1wRRs',	'Great British Insulation Scheme (GBIS)','Ofgem - Great British Insulation Scheme (GBIS)'),
+('2nAxHa72OqhE6eKymHZIx-sV3vI',	'Apprenticeship provider and assessment register service','NAS - Apprenticeship Provider and Assessment Register Service'),
+('MJ8nBsh32LHweUjb6x3p7qf-_TE',	'Apprenticeship assessment service','NAS - Apprenticeship Assessment Service'),
+('txsGLvMYYCPaWPZRq2L7XxEnyro',	'Early Years Child Development Training','DFE - Early Years Child Development Training');
