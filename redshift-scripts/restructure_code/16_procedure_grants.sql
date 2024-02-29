68666a293f74906a1372813f5ac4270ce5e12e84
psodhi-gds <paramjeet.sodhi@digital.cabinet-office.gov.uk>
2024-02-29 13:49:21 +0000

    update to ref1

--- redshift-scripts/restructure_code/16_procedure_grants.sql
+++ redshift-scripts/restructure_code/16_procedure_grants.sql
@@ -8,3 +8,17 @@ GRANT EXECUTE ON procedure conformed_refactored.event_extensions_refactored_upse
 GRANT EXECUTE ON procedure conformed_refactored.fact_user_journey_event_refactored_upsert() TO "IAMR:dev-dap-redshift-processing-role";
 GRANT EXECUTE ON procedure conformed_refactored.redshift_date_dim() TO "IAMR:dev-dap-redshift-processing-role";
 GRANT EXECUTE ON procedure conformed_refactored.update_event_batch_table() TO "IAMR:dev-dap-redshift-processing-role";
+
+GRANT ALL ON DATABASE "dap_txma_reporting_db_refactored" TO GROUP dap_elt_processing;
+GRANT ALL ON SCHEMA "conformed_refactored" TO GROUP dap_elt_processing;
+GRANT ALL ON ALL TABLES IN SCHEMA "conformed_refactored" TO GROUP dap_elt_processing;
+GRANT USAGE ON SCHEMA "dap_txma_stage" TO GROUP dap_elt_processing;
+
+
+/*
+Database object privileges to group
+*/
+
+GRANT ALL ON DATABASE "dap_txma_reporting_db_refactored" TO GROUP dap_elt_processing;
+GRANT ALL ON SCHEMA "audit_refactored" TO GROUP dap_elt_processing;
+GRANT ALL ON ALL TABLES IN SCHEMA "audit_refactored" TO GROUP dap_elt_processing;
